da63fd53a90196cf2e2921af117facde1f1cfa4a
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-19 19:41:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,6 @@
 def oxford_comma(array)
   if array.length <= 2
     array.join(" and ")
-  elsif array.length == 3
-    "#{array[0]}, #{array[1]}, and #{array[2]}"
   else
     array[-1] = "and #{array[-1]}"
     array.join(", ")
